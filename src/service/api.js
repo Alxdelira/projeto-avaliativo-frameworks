3d18e12e07ada4396cea1e62a2ceadb9cb534326
alxdelira <alx.delira@gmail.com>
2023-10-23 23:45:49 -0400

    teste de deploy

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function setupAPIClient(ctx) {
     const api = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_DBURL || "https://api-eventos-server-git-main-alxdelira.vercel.app/",
+        baseURL:"https://api-eventos-server-git-main-alxdelira.vercel.app/",
         headers: {
             Accept: "application/json"
         }
